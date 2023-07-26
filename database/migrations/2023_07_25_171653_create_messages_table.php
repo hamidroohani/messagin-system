7551473c083b1d385867123db4d6077ff9bf06a5
hamid roohani <hamid.roohany@gmail.com>
2023-07-26 08:45:11 +0330

    Fix the foreign keys issues

--- database/migrations/2023_07_25_171653_create_messages_table.php
+++ database/migrations/2023_07_25_171653_create_messages_table.php
@@ -15,14 +15,17 @@ return new class extends Migration
     {
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\User::class, 'sender_id')->constrained()->onDelete('cascade');
-            $table->foreignIdFor(\App\Models\User::class, 'receiver_id')->constrained()->onDelete('cascade');
+            $table->unsignedBigInteger('sender_id');
+            $table->unsignedBigInteger('receiver_id');
             $table->text('body')->nullable();
             $table->string('status');
             $table->dateTime('sent_at')->nullable();
             $table->string('ref_code')->nullable();
             $table->string('err_msg')->nullable();
             $table->timestamps();
+
+            $table->foreign('sender_id')->references('id')->on('users');
+            $table->foreign('receiver_id')->references('id')->on('users');
         });
     }
 
