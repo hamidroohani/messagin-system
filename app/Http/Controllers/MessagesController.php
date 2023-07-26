4c664a15d8c75e445bdaa7e8e492f617c2044194
hamid roohani <hamid.roohany@gmail.com>
2023-07-26 14:49:21 +0330

    add some comments

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -20,11 +20,17 @@ class MessagesController extends Controller
     }
     public function index(Request $request)
     {
+        // get the chat screen based on entry
         $participant = $request->input('email');
         $participant = $this->userRepository->find_by_email($participant);
+
+        //those are for the sidebar
         $last_messages = $this->messageRepository->last_messages();
         $last_saved_message = $this->messageRepository->last_saved_message();
+
+        // get the main chat messages
         $messages = $this->messageRepository->get_direct_messages($participant->id);
+        
         return view('pages.messages', compact('last_messages', 'last_saved_message', 'messages', 'participant'));
     }
 }
